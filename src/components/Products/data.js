f6e1f783b41ebdabc84c81bdb1d59c2fcd8fb76d
yasmin <yasmin.mouawad@gmail.com>
2021-01-16 02:47:43 +0200

    ecommerce restaurant

---
+++ src/components/Products/data.js
@@ -0,0 +1,62 @@
+import product1 from '../../images/product1.jpg';
+import product2 from '../../images/product2.jpg';
+import product3 from '../../images/product3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+
+export const productData = [
+   {
+    img: product1,
+    alt:'Pizza',
+    name:'Supreme Pizza',
+    desc:'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price:'$19.99',
+    button:'Add to cart'
+   },
+   {
+    img: product2,
+    alt:'Pizza',
+    name:'Supreme Pizza',
+    desc:'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price:'$15.99',
+    button:'Add to cart'
+   },
+   {
+    img: product3,
+    alt:'Pizza',
+    name:'Supreme Pizza',
+    desc:'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price:'$22.99',
+    button:'Add to cart'
+   }
+
+];
+
+export const productDataTwo = [
+   {
+    img: sweet2,
+    alt:'Dounts',
+    name:'Doughlicious',
+    desc:'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price:'$19.99',
+    button:'Add to cart'
+   },
+   {
+    img: sweet3,
+    alt:'Dounts',
+    name:'Doughlicious',
+    desc:'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price:'$15.99',
+    button:'Add to cart'
+   },
+   {
+    img: sweet1,
+    alt:'Dounts',
+    name:'Doughlicious',
+    desc:'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price:'$22.99',
+    button:'Add to cart'
+   }
+
+]
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,23 @@
+import React  from 'react';
+import {SidebarContainer, Icon, CloseIcon, SidebarMenu, SidebarLink, SidebarWrap, SidebarRoute} from './SidebarElements';
+
+const Sidebar = ({isOpen, toggle}) =>{
+ 
+    return(
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon/>
+            </Icon>
+            <SidebarMenu>
+                <SidebarLink to="/">Pizzas</SidebarLink>
+                <SidebarLink to="/">Desserts</SidebarLink>
+                <SidebarLink to="/">Full Menu</SidebarLink>
+            </SidebarMenu>
+            <SidebarWrap>
+               <SidebarRoute to="/">Order Now</SidebarRoute>
+            </SidebarWrap>
+        </SidebarContainer>
+    );
+
+};
+export default Sidebar;
